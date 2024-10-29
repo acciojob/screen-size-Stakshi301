38ccb889b45d911246284c4de07eb27b71e8b740
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-29 14:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ function updateWindowSize() {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      
+      const sizeInfoElement = document.querySelector("#sizeInfo h1");
+      sizeInfoElement.textContent = `Width: ${width} and Height: ${height}`;
+    }
+    updateWindowSize();
+
+    window.addEventListener("resize", updateWindowSize);
